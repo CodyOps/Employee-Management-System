ada325f382a9ce1286fd52678d33fc53ad2f8384
Cody Chen <codychen1@gmail.com>
2020-12-20 11:31:44 -0700

    added values to sql db

--- employee.sql
+++ employee.sql
@@ -1,30 +1,35 @@
+DROP DATABASE IF EXISTS employee_managementDB;
 CREATE DATABASE employee_managementDB;
 
 USE employee_managementDB;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
- 
+  name VARCHAR(45)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  title VARCHAR(45),
+  salary DECIMAL (10, 2),
+  department_id INT 
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
   manager_id INT,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
-
+  role_id INT
 );
 
-SELECT * FROM employee_managementDB;
+INSERT INTO department (name)
+VALUES ("Human Resources"), ("Research and Development"), ("Engineering"), ("Accounting"), ("Sales"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("recruiter", 57500, 1), ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4), ("sales person", 85650, 5), ("lawyer", 190000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "Doe", 1, 3), ("Mike", "Chan", 5, 1), ("Ashley", "Rodriguez", 3, null), ("Kevin", "Tupik", 3, 3), ("Malia", "Brown", 4, null), ("Sarah", "Lourd", 6, null);
+
+SELECT * FROM employee;
